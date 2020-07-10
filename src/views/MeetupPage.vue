45d6d7a4f6ac7b860396dfe1210bf41643484d2f
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-07-10 10:58:36 +0200

    part-4: routing params

--- src/views/MeetupPage.vue
+++ src/views/MeetupPage.vue
@@ -3,7 +3,11 @@
         <h1>
             Meetup Page
         </h1>
-        <p></p>
+        <p>{{ $route.fullPath }}</p>
+        <p>{{ $route.hash }}</p>
+        <p>{{ $route.path }}</p>
+        <p>{{ $route.query }}</p>
+        <p>{{ $route.params }}</p>
     </div>
     
 </template>
