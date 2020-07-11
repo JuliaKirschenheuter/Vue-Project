a18bb037742fbad03b35c5e103786ebdafa838c6
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-07-11 12:12:22 +0200

    part-4: meta property was added to route: error was fixed

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,43 @@
 <template>
-  <div id="app">
-    <div class="wrapper bg-grey">
-      <header class="header">
-        <div>
-          <h1>
-            <router-link
-                    v-if="$route.meta.showBackToList"
-                    :to="{name: 'index'}">
-              <img src="@/assets/logo.svg" alt="Meetups" />
-            </router-link>
-          </h1>
+    <div id="app">
+        <div class="wrapper bg-grey">
+            <header class="header">
+                <div>
+                    <h1>
+                        <router-link
+                                :to="{name: 'index'}">
+                            <img alt="Meetups" src="@/assets/logo.svg"/>
+                        </router-link>
+                    </h1>
+                </div>
+                <nav>
+                    <router-link :to="{name: 'meetups-list'}" v-if="$route.meta.showBackToList"> &larr; Вернуться к
+                        списку
+                    </router-link>
+                    <a href="#">Вход</a>
+                    <a href="#">Регистрация</a>
+                    <a href="#">Создать митап</a>
+                </nav>
+            </header>
+            <main class="main">
+                <router-view></router-view>
+            </main>
+            <footer class="footer">
+                <div class="container">
+                    2020. learn JS
+                </div>
+            </footer>
         </div>
-        <nav>
-          <router-link :to="{name: 'meetups-list'}"> &larr; Вернуться к списку</router-link>
-          <a href="#">Вход</a>
-          <a href="#">Регистрация</a>
-          <a href="#">Создать митап</a>
-        </nav>
-      </header>
-      <main class="main">
-        <router-view></router-view>
-      </main>
-      <footer class="footer">
-        <div class="container">
-          2020. learn JS
-        </div>
-      </footer>
     </div>
-  </div>
 </template>
 
 
 <script>
 
-  export default {
-    name: 'App',
-    components: {
-    },
-  };
+    export default {
+        name: 'App',
+        components: {},
+    };
 </script>
 
 <style></style>
